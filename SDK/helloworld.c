9311848bdce7c10100684aa927c3e2cd79c2b8a7
Arun D A <d.aarun@yahoo.in>
2021-12-09 14:05:17 +0530

    Beautified sw code

--- SDK/helloworld.c
+++ SDK/helloworld.c
@@ -142,20 +142,10 @@ float kalmanfilter(float z)
 
 int main()
 {
-    init_platform();
+  init_platform();
 
-    XCounter xc;
-    XCounter* xcptr = &xc;
-
-    /*
-    char buff[11];
-    static float arr[9] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
-
-	union{
-	unsigned int a;
-	float b;
-	}itof;
-	*/
+  XCounter xc;
+  XCounter* xcptr = &xc;
 
 	XCounter_Initialize(xcptr, 0);
 	XCounter_EnableAutoRestart(xcptr);
@@ -182,32 +172,6 @@ int main()
 	xil_printf("e: %d\n\r",error);
 	xil_printf("c: %d\n\r",t2-t1);
 
-	/*
-
-	t1 = XCounter_Get_return(xcptr);
-
-
-	for(int i=0;i<9;i++)
-	{
-		itof.b=arr[i];
-		Xil_Out32 (XPAR_BRAM_0_BASEADDR+(4*i),itof.a); //Sending the arr[] values to BRAM
-	}
-
-	Xil_Out32 ((XPAR_SUM_ARR_0_S_AXI_CONTROL_BASEADDR+0x18), XPAR_BRAM_0_BASEADDR); //Sending image pointer address using offset
-
-	Xil_Out32 ((XPAR_SUM_ARR_0_S_AXI_CONTROL_BASEADDR+0x00), 1);	//Start the IP
-
-	while (0 == (2 & Xil_In32((XPAR_SUM_ARR_0_S_AXI_CONTROL_BASEADDR+0x00))));	//To check IP is done or not
-
-	itof.a=Xil_In32 ((XPAR_SUM_ARR_0_S_AXI_CONTROL_BASEADDR + 0x10));	//Receiving returned value from HLS IP (converting because processor handles uint data) using address offset for returned argument
-
-	t2 = XCounter_Get_return(xcptr);
-
-	sprintf(buff,"%f ",itof.b);
-	xil_printf("cb: %d\n\r",t2-t1);
-	xil_printf("res: %s\n\r",buff);
-	*/
-
-    cleanup_platform();
-    return 0;
+  cleanup_platform();
+  return 0;
 }
